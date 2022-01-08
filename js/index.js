a6d54b24385051dfe2a358a00fe6817519f616fa
mrcinzza <mrcinzza@gmail.com>
2022-01-08 14:25:19 +0100

    Working environment setup

---
+++ js/index.js
@@ -0,0 +1,13 @@
+gridGenerator()
+
+
+
+
+// Functions
+
+function gridGenerator() {
+    let squareNumber = 0;
+    for (let i = 0; i < 24; i++) {
+        document.querySelector(".grid-container").innerHTML += "<div class=\'grid-item\'>1</div>";
+    }
+}
