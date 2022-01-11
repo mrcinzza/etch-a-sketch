d4915436fe1cf11318480b655facd2d55ffae062
mrcinzza <mrcinzza@gmail.com>
2022-01-11 23:30:08 +0100

    Added a logic to the js file moseOver gridGenerator Those are the functions that were created

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
-gridGenerator()
+const gridContainer = document.getElementById("grid-container");
+const gridItem = document.querySelectorAll(".grid-item");
+
+let defaultSize = 16;
+
+function gridGenerator(rows, cols) {
+    gridContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`
 
 
-
-
-// Functions
-
-function gridGenerator() {
-    let squareNumber = 0;
-    for (let i = 0; i < 24; i++) {
-        document.querySelector(".grid-container").innerHTML += "<div class=\'grid-item\'>1</div>";
+    for (let i = 0; i < (rows * cols); i++) {
+        gridContainer.innerHTML += "<div id='grid-item' class='grid-item'></div>";
     }
+}
+
+function mouseOver() {
+    document.querySelectorAll(".grid-item").forEach(
+        gridItem => gridItem.addEventListener("mouseover", e => e.target.classList.add("grid-item-black"))
+    );
+}
+
+window.onload = () => {
+    gridGenerator(defaultSize, defaultSize);
+    mouseOver();
 }
